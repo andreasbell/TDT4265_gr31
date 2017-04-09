d493a422c1ea6e3f01fddd086442f015536a39b7
Andreas Bell Martinsen <andreas.bell@hotmail.com>
2017-04-09 14:36:10 +0200

    update

--- main.py
+++ main.py
@@ -1,32 +1,3 @@
-from DLClassifier import DLClassifier
-from CVClassifier import CVClassifier
-import numpy as np
-import cv2
-
-#det = DLClassifier("weights/detector.ckpt")
-det = CVClassifier(False)
-
-np.set_printoptions(precision=1, suppress=True)
-
-cap = cv2.VideoCapture(0)
-
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    
-    #Detect objects
-    det.detect_from_cvmat(frame)
-    #res = det.show_results(frame,yolo.result)
-    #print(det.result)
-    print(str(det.result) + " "*20, sep=' ', end='\r', flush=True)
-
-    # Display the resulting frame
-    #cv2.imshow('frame: ',frame)
-    cv2.imshow('frame: ',det.image)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
-
+from Game import RPS_Game
+app = RPS_Game()
+app.run()
